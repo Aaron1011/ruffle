6034e9c1f1aca7187df11ec9ccae4570eaae33f8
Nathan Adams <dinnerbone@dinnerbone.com>
2022-09-07 03:33:30 +0200

    wgpu: Removed Descriptors::pipelines, we don't need 3 pipelines

--- render/wgpu/src/backend.rs
+++ render/wgpu/src/backend.rs
@@ -402,7 +402,7 @@ impl<T: RenderTarget> WgpuRenderBackend<T> {
                         self.descriptors
                             .device
                             .create_bind_group(&wgpu::BindGroupDescriptor {
-                                layout: &self.descriptors.pipelines.gradient_layout,
+                                layout: &self.descriptors.onscreen.pipelines.gradient_layout,
                                 entries: &[
                                     wgpu::BindGroupEntry {
                                         binding: 0,
@@ -477,7 +477,7 @@ impl<T: RenderTarget> WgpuRenderBackend<T> {
                         self.descriptors
                             .device
                             .create_bind_group(&wgpu::BindGroupDescriptor {
-                                layout: &self.descriptors.pipelines.bitmap_layout,
+                                layout: &self.descriptors.onscreen.pipelines.bitmap_layout,
                                 entries: &[
                                     wgpu::BindGroupEntry {
                                         binding: 0,
@@ -613,7 +613,7 @@ impl<T: RenderTarget + 'static> RenderBackend for WgpuRenderBackend<T> {
                 self.descriptors
                     .device
                     .create_bind_group(&wgpu::BindGroupDescriptor {
-                        layout: &self.descriptors.pipelines.bitmap_layout,
+                        layout: &self.descriptors.onscreen.pipelines.bitmap_layout,
                         entries: &[
                             wgpu::BindGroupEntry {
                                 binding: 0,
@@ -854,7 +854,7 @@ impl<T: RenderTarget + 'static> RenderBackend for WgpuRenderBackend<T> {
             .descriptors
             .device
             .create_bind_group(&wgpu::BindGroupDescriptor {
-                layout: &self.descriptors.pipelines.bitmap_layout,
+                layout: &self.descriptors.onscreen.pipelines.bitmap_layout,
                 entries: &[
                     wgpu::BindGroupEntry {
                         binding: 0,
